f7a6f93cf158cd821e96906b9467463c77860a7a
Sebastiaan Westdijk <sebastiaan.westdijk@eneco.com>
2017-11-01 10:11:22 +0100

    Powershell scripts for moving disks and setting up new VM

---
+++ move_datadisks.ps1
@@ -0,0 +1,30 @@
+﻿#Provide the subscription Id of the subscription where managed disk exists
+$sourceSubscriptionId='f066eaf1-e8fe-4457-a69d-148a62a7c682'
+
+#Provide the name of your resource group where managed disk exists
+$sourceResourceGroupName='VM_W10'
+
+#Provide the name of the managed disk
+$managedDiskName='Seabass_OsDisk_1_c1614d82d0dc42f18a8c6ab3cdb4faca'
+
+#Set the context to the subscription Id where Managed Disk exists
+Select-AzureRmSubscription -SubscriptionId $sourceSubscriptionId
+
+#Get the source managed disk
+$managedDisk= Get-AzureRMDisk -ResourceGroupName $sourceResourceGroupName -DiskName $managedDiskName
+
+#Provide the subscription Id of the subscription where managed disk will be copied to
+#If managed disk is copied to the same subscription then you can skip this step
+$targetSubscriptionId='332990a9-6bb3-4e2d-9875-a996fe03c894'
+
+#Name of the resource group where snapshot will be copied to
+$targetResourceGroupName='VM_W10'
+
+#Set the context to the subscription Id where managed disk will be copied to
+#If snapshot is copied to the same subscription then you can skip this step
+Select-AzureRmSubscription -SubscriptionId $targetSubscriptionId
+
+$diskConfig = New-AzureRmDiskConfig -SourceResourceId $managedDisk.Id -Location $managedDisk.Location -CreateOption Copy 
+
+#Create a new managed disk in the target subscription and resource group
+New-AzureRmDisk -Disk $diskConfig -DiskName $managedDiskName -ResourceGroupName $targetResourceGroupName
---
+++ setup_VM_on_existing_datadisk.ps1
@@ -0,0 +1,39 @@
+﻿#Create Resource Group
+$resourceGroupName = "VM"
+$resourceGroup = New-AzureRmResourceGroup -Name $resourceGroupName -Location "westeurope" 
+
+# Select Vritual Network
+$virtualNetworkName = "MyVirtualNetwork"
+$locationName = "westeurope"
+
+$susbnet = New-AzureRmVirtualNetworkSubnetConfig -Name FrontEnd -AddressPrefix 10.0.1.0/24 
+
+$virtualNetwork = New-AzureRmVirtualNetwork -Name $virtualNetworkName `
+                                            -Subnet $susbnet `
+                                            -Location $locationName `
+                                            -ResourceGroupName $resourceGroupName `
+                                            -AddressPrefix 10.0.0.0/16                                       
+
+                                            
+# Create IP-Adres
+$publicIp = New-AzureRmPublicIpAddress -Name "MyPublicIpAddress" `
+                                       -ResourceGroupName $resourceGroupName `
+                                       -Location $locationName -AllocationMethod Dynamic
+
+# Create Network Interface
+$networkInterface = New-AzureRmNetworkInterface -ResourceGroupName $resourceGroupName `
+                                                -Name "MyNetworkInterface"  `
+                                                -Location $locationName `
+                                                -SubnetId $virtualNetwork.Subnets[0].Id `
+                                                -PublicIpAddressId $publicIp.Id `
+                                                
+
+# VM Config
+$vhdUri = "https://mytargetstorageaccount.blob.core.windows.net/migratedvhds/myvhd.vhd"
+$vmConfig = New-AzureRmVMConfig -VMName "MyVirtualMachine" -VMSize "Standard_A1"
+$vmConfig = Set-AzureRmVMOSDisk -VM $vmConfig -Name "MyVMDisk" -VhdUri $vhdUri `
+                                -CreateOption Attach -Windows
+$vmConfig = Add-AzureRmVMNetworkInterface -VM $vmConfig -Id $networkInterface.Id
+
+#Create VM
+$vm = New-AzureRmVM -VM $vmConfig -Location $locationName -ResourceGroupName $resourceGroupName
